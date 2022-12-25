e2931424adf3768ede44be6ddef747ec89b6960c
Hammad Malik <hammadmalik5353@gmail.com>
2022-12-25 14:13:34 +0500

    update

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 app = Flask(__name__)
-cwd = os.getcwd()
-print(cwd)
+abs = os.path.abspath('main.py')
+print(abs)
 
 training = pd.read_csv('Data/Training.csv')
 testing= pd.read_csv('Data/Testing.csv')
